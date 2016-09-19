76065ee2d5133f29b5451d87da34544727e99d07
jspirovski <jovica.spirovski@gmail.com>
2016-09-19 15:28:22 +0200

    Just to try

--- src/jspirovski_dummy_package/HelloWorld.java
+++ src/jspirovski_dummy_package/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World! Here I am :)");
+        System.out.println("blablabla");
 
     }
 }
