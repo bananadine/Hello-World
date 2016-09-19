8185c1e0fc65c49264baf2f84449c457c6e3dfe0
Teodora <cvetanovskateodora@gmail.com>
2016-09-19 13:42:42 +0200

    New package created

---
+++ src/teodoraC/SimpleHello.java
@@ -0,0 +1,10 @@
+package teodoraC;
+
+/**
+ * Created by tcvetanovska on 9/19/2016.
+ */
+public class SimpleHello {
+    public static void main(String[] args) {
+        System.out.println("My job here is done.");
+    }
+}
