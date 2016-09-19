ddb3e6a8abb1ec88d721b0887a7a5cfb7e2e24c3
jspirovski <jovica.spirovski@gmail.com>
2016-09-19 15:24:09 +0200

    New package created.

---
+++ src/jspirovski_dummy_package/HelloWorld.java
@@ -0,0 +1,12 @@
+package jspirovski_dummy_package;
+
+/**
+ * Created by jspirovski on 9/19/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello World! Here I am :)");
+
+    }
+}
