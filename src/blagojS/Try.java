83a1bbb6aee2f4e11eb862e371318d0d98a5e02f
jshalamanoski <jovsal@outlook.com>
2016-09-19 15:43:14 +0200

    Adding methods to Try class

--- src/blagojS/Try.java
+++ src/blagojS/Try.java
@@ -2,4 +2,28 @@ package blagojS;
 
 
 public class Try {
+
+    int id;
+    String name;
+
+    public Try(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
