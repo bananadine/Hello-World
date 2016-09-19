667ed44eaf091faadbce833949f17b770a72b89c
Maksimilijan Sarajlija <msarajlija@endava.com>
2016-09-19 13:12:37 +0200

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by msarajlija on 9/19/2016.
+ */
+public class HelloWorld  {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello World!");
+    }
+}
