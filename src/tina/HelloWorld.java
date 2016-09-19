b8110e28152c770a5de9e2f98c221c920e073e89
Christina Nastevska <hristinanastevska@gmail.com>
2016-09-19 13:22:41 +0200

    Tina

---
+++ src/tina/HelloWorld.java
@@ -0,0 +1,11 @@
+package tina;
+
+/**
+ * Created by hnastevska on 9/19/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello World!");
+    }
+}
